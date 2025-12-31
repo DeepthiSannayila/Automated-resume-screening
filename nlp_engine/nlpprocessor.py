724cb3c4bf2c29bacf032d2c0bbcbd6053e54fb8
Deepthi <deepthisannayila@gmail.com>
2025-12-31 12:08:24 +0530

    Fix spaCy model loading on Streamlit Cloud

--- nlp_engine/nlpprocessor.py
+++ nlp_engine/nlpprocessor.py
@@ -1,8 +1,8 @@
 import spacy
-from spacy.cli import download
 
 try:
     nlp = spacy.load("en_core_web_sm")
-except OSError:
-    download("en_core_web_sm")
+except:
+    import subprocess
+    subprocess.run(["python", "-m", "spacy", "download", "en_core_web_sm"])
     nlp = spacy.load("en_core_web_sm")
